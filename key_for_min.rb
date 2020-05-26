f9dd12fc14e75d9f4b18d82c0a7d2824e7d3ea2d
Shiro Han <shan99business@gmail.com>
2020-05-26 03:06:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_val = 9999999999999999999999999999999999999999999
   output = "nil"
-  
+
   name_hash.each do |key, value|
     if value < min_val
       value = min_val
